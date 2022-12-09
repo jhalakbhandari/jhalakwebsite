8cb82a5888abc30c9768796d9e0b793f13d412ab
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2022-12-09 15:49:34 +0530

    deploying

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,10 @@ const Footer = () => {
     <footer className="bg-gray-900  px-4  py-7 space-between  text-white inset-x-0 bottom-0 p-2 text-xs text-center">
       <div className="pb-2">&copy; Copyright 2022</div>
       <div className=" md:flex md:Justify-between  md:items-center sm:px-12 space-x-4 flex justify-center">
-        <a className="text-white" href="#">
+        <a
+          className="text-white"
+          href="https://www.linkedin.com/in/jhalak-bhandari-3680961aa/"
+        >
           <InstagramIcon />
         </a>
         <a
